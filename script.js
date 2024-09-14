06bc42164607c310e6f2cb6acaf98b190b3eaaaf
CDevs <constantsuchet@gmail.com>
2024-09-14 20:28:39 +0200

    first footer commit, first version

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 window.addEventListener('scroll', function() {
   var header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
-});
+  var footer = document.querySelector('footer');
+  var scrollPosition = window.pageYOffset;
+  var windowHeight = window.innerHeight;
+  var bodyHeight = document.body.offsetHeight;
 
+  header.classList.toggle('sticky', scrollPosition > 0);
+
+  // Check if the user has scrolled to the bottom
+  if (scrollPosition + windowHeight >= bodyHeight - 100) {
+    footer.classList.add('visible');
+  } else {
+    footer.classList.remove('visible');
+  }
+});
