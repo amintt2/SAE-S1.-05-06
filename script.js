9e9b508742d24501d5607ece4dd32d8326f9212a
Constant <constantsuchet@gmail.com>
2024-09-14 22:21:21 +0200

    added page creator on the footer

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ window.addEventListener('scroll', function() {
     footer.classList.remove('visible');
   }
 });
+
+function updateFooterText() {
+  // Get the current page URL path
+  var path = window.location.pathname;
+
+  // Select the footer element
+  var footer = document.getElementById("creator"); // Assuming your footer text has this ID
+
+  // Check which page the user is on and change the footer text
+  if (path.includes("index")) {
+    footer.innerHTML = "Createur de la page : Constant SUCHET";
+  } else if (path.includes("Innovations")) {
+    footer.innerHTML = "Createur de la page : Talali Zakaria";
+  } else if (path.includes("Finances")) {
+    footer.innerHTML = "Createur de la page : Touzi Tahar Amin";
+  } else if (path.includes("Evolution")) {
+    footer.innerHTML = "Createur de la page : Jibryl Tebib";
+  } else {
+    footer.innerHTML = "Createur de la page : Constant SUCHET";
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', updateFooterText);
